34f4f38ae4b800fdfe44e34e04a8fa768295fff3
root <root@localhost.localdomain>
2016-12-04 12:30:10 -0500

    Update Regex.

--- match.cpp
+++ match.cpp
@@ -3,14 +3,16 @@
 #include <boost/regex.hpp>
 
 // On/Off Switch For Dubugging
-#define _Debug 0
+#define _Debug 1
 
 // ======================= 전역 변수 =========================
 boost::smatch m;                        /* Matches */
 boost::regex URL( ".*Host\: ([^\\r]*).*" );                                                 // m[1]
 boost::regex Cookie( ".*Cookie\: ([^\\r]*).*" );                                            // m[1]
-boost::regex User_ID( ".*\<user_id\>\<\!\\[\\w+\\[(\.+)\\]\\]\>\<\/user_id\>.*" );          // m[1]
-boost::regex Password( ".*\<password\>\<\!\\[\\w+\\[(\.+)\\]\\]\>\<\/password\>.*" );       // m[1]
+boost::regex User_ID( ".*(?:\\_|\\&|^)(?:M_ID|id|Id|userId|login)\=([^\\&]*).*" );          // m[1]
+//boost::regex User_ID( ".*\<user_id\>\<\!\\[\\w+\\[(\.+)\\]\\]\>\<\/user_id\>.*" );        // m[1]
+boost::regex Password( ".*(?:\\&pw|pwd|M_PWD|password|Password)\=([^\\r\\&]*).*" );         // m[1]
+//boost::regex Password( ".*\<password\>\<\!\\[\\w+\\[(\.+)\\]\\]\>\<\/password\>.*" );     // m[1]
 
 const int HTTP_REQUEST = 0;            /* Packet types */
 const int FTP = 1;
